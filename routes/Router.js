e442ef5aa1cf0b730f729c0f04af8013b5dba4b2
GabiDante <gabi.a.dante@gmail.com>
2022-09-12 18:48:45 -0300

    validando desde el back

--- routes/Router.js
+++ routes/Router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Validation = require('../middlewares/validation');
-const Controller = require('../controllers/Controller')
+const Controller = require('../controllers/Controller');
+const validation = require('../middlewares/validation');
 
 router.get('/', Controller.index); 
 router.get('/create', Controller.create); 
-router.post('/create', Controller.createProduct); 
+router.post('/create', validation, Controller.createProduct); 
 
 
 
