9d8f67edfb19558f4e389265d22826d714e99034
GabiDante <gabi.a.dante@gmail.com>
2022-09-13 09:08:50 -0300

    apicontroller , apirouter

--- routes/APIrouter.js
+++ routes/APIrouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Controller = require('../controllers/APIcontroller')
+const APIcontroller = require('../controllers/APIcontroller')
 
 
 //Aca van las rutas de tu API
-
+router.get('/products', APIcontroller.list);
+router.post('/products', APIcontroller.store);
+router.get ('/products/:id', APIcontroller.show);
+router.delete ('/products/:id', APIcontroller.delete);
+//router.get('/brand',APIcontroller.brandProducts)
 
 
 module.exports = router;
